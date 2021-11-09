8dd72f58f9db8e496a1893a3ab4550ecec768996
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-11-09 18:43:51 +0900

    feat: main api

--- main.go
+++ main.go
@@ -2,48 +2,12 @@ package main
 
 import (
 	"fmt"
+	"go-todo-app/app/controllers"
 	"go-todo-app/app/models"
 )
 
 func main() {
-	//fmt.Println(config.Config.Port)
-	//fmt.Println(config.Config.SQLDriver)
-	//fmt.Println(config.Config.DbName)
-	//fmt.Println(config.Config.LogFile)
-	//
-	//log.Println("test")
 	fmt.Println(models.Db)
-	//
-	//u := &models.User{}
-	//u.Name = "Hiroshi"
-	//u.Email = "hiroshi@example.com"
-	//u.PassWord = "testtest"
-	//fmt.Println(u)
 
-	//u.CreateUser()
-	//
-	//user, _ := models.GetUser(2)
-	//user.CreateTodo("CD返却")
-
-	//fmt.Printf("%+v\n", u)
-	//
-	//u.Name = "Takeshi Emoto"
-	//u.Email = "test3@example.com"
-	//u.UpdateUser()
-	//u, _ = models.GetUser(2)
-	//fmt.Printf("%+v\n", u)
-	//u.DeleteUser()
-
-	//t, _ := models.GetTodo(1)
-	//fmt.Println(t)
-
-	//todos, _ := models.GetTodos()
-	//fmt.Println(todos)
-
-	//u2, _ := models.GetUser(2)
-	//todos, _ := u2.GetTodosByUser()
-	//for _, v := range todos {
-	//	fmt.Println(v)
-	//}
-
+	controllers.StartMainServer()
 }
